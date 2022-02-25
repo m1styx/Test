43bd49d1766cc60157765c8d2bbbde1d5c903829
matvejspesivcev@gmail.com <matvejspesivcev@gmail.com>
2022-02-25 19:05:22 +0100

    first commitegit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Test
+{
+    class StringCounter
+    {
+        public static void Main()
+        {
+            Console.WriteLine("напишите текст");
+
+            string? s = Console.ReadLine();
+
+            if (s == null || s == "")
+            {
+                Console.WriteLine("Напишите что-то");
+            }
+            else
+            {
+                Console.WriteLine("{0}", s.Length);
+
+            }
+
+        }
+    }
+}
